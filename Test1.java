f3c1bc6bcf342f62ba0c386cb7e9f8bc2bbbafaf
Raonak Islam Prova <raonakprova154@gmail.com>
2022-01-24 15:35:39 +0600

    initial commit Test1

---
+++ Test1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test1 
+{
+      public static void main(String[] args)
+      {
+         Scanner input = new Scanner(System.in);
+         System.out.println("Please enter a number");
+         int number = input.nextInt();
+         System.out.println("number= "+number);
+         
+      }
+
+}
